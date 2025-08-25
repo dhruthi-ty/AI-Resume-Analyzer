f5479f2a57929ee8e9da5f84ee1e150f5890d8ca
dhruthi-ty <dhruthity99@gmail.com>
2025-08-25 13:00:13 +0530

    upload the file and analysis of it and feedback

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes"; 
+import { stat } from "fs";
 export default [
     index("routes/home.tsx"),
-    route('/auth','routes/auth.tsx'),] satisfies RouteConfig;
+    route('/auth','routes/auth.tsx'),
+    route('/upload','routes/upload.tsx'),
+] satisfies RouteConfig;
+    
